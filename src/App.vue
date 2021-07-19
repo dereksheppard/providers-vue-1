32768754b0c14085e7b3ebd59adcd116dc817c92
Derek Sheppard <dereksheppard@gmail.com>
2021-07-19 10:26:35 -0700

    Changed component name to ProvidersLookup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <ProvidersLookup/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/ProvidersLookup.vue'
+import ProvidersLookup from './components/ProvidersLookup.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProvidersLookup
   }
 }
 </script>
